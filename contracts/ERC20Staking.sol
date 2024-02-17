656405c4c15b5e6ac67878019e1cf4ef13b79709
Dannyswiss <dannyswiss9ija@gmail.com>
2024-02-17 15:19:07 +0100

    added constrctor

--- contracts/ERC20Staking.sol
+++ contracts/ERC20Staking.sol
@@ -1,2 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract ERC20Staking {
+
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable token;
+
+    constructor() {
+        token = _token;
+    }
+}
